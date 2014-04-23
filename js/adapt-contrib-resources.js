41cdf31c9675ae39bfdd49b13d0ba84d30c51702
Daryl Hedley <darylhedley@hotmail.com>
2014-04-23 11:03:50 +0100

    Added resources files

---
+++ js/adapt-contrib-resources.js
@@ -0,0 +1,50 @@
+/*
+* Page Level Progress
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Daryl Hedley <darylhedley@hotmail.com>
+*/
+
+define(function(require) {
+
+	var Adapt = require('coreJS/adapt');
+	var Backbone = require('backbone');
+	var Drawer = require('coreJS/drawer');
+	var ResourcesView = require('extensions/adapt-contrib-resources/js/adapt-contrib-resourcesView');
+	var ResourcesHelpers = require('extensions/adapt-contrib-resources/js/adapt-contrib-resourcesHelpers');
+
+	function setupResources(resourcesModel, resourcesItems) {
+
+		var resourcesCollection = new Backbone.Collection(resourcesItems);
+		var resourcesModel = new Backbone.Model(resourcesModel);
+
+		Adapt.on('resources:showResources', function() {
+			Adapt.drawer.triggerCustomView(new ResourcesView({
+				model: resourcesModel, 
+				collection: resourcesCollection
+			}).$el);
+		});
+	
+	}
+
+	Adapt.once('app:dataReady', function() {
+
+		var courseResources = Adapt.course.get('_resources');
+
+		if (courseResources) {
+			var drawerObject = {
+		        title: courseResources.title,
+		        description: courseResources.description,
+		        className: 'resources-drawer'
+		    };
+		    // Syntax for adding a Drawer item
+		    // Adapt.drawer.addItem([object], [callbackEvent]);
+		    Adapt.drawer.addItem(drawerObject, 'resources:showResources');
+		} else {
+			console.log('Sorry, no resources object is set on the course.json file');
+		}
+
+		setupResources(courseResources, courseResources._resourcesItems);
+
+	});
+
+})
