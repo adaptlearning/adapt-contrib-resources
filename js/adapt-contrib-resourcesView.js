2f950aefae78979337aa08dde0f2eb8ae14e4aa5
Oliver Foster <oliver.foster@kineo.com>
2015-06-03 14:51:09 +0100

    Extended resources accessiblity

--- js/adapt-contrib-resourcesView.js
+++ js/adapt-contrib-resourcesView.js
@@ -1,51 +1,56 @@
 define(function(require) {
 
-	var Backbone = require('backbone');
-	var Adapt = require('coreJS/adapt');
+    var Backbone = require('backbone');
+    var Adapt = require('coreJS/adapt');
 
-	var ResourcesView = Backbone.View.extend({
+    var ResourcesView = Backbone.View.extend({
 
-		className: "resources",
+        className: "resources",
 
-		initialize: function() {
-			this.listenTo(Adapt, 'remove', this.remove);
-			this.render();
-		},
+        initialize: function() {
+            this.listenTo(Adapt, 'remove', this.remove);
+            this.render();
+        },
 
-		events: {
-			'click .resources-filter a': 'onFilterClicked'
-		},
+        events: {
+            'click .resources-filter a': 'onFilterClicked'
+        },
 
-		render: function() {
-			var collectionData = this.collection.toJSON();
-			var modelData = this.model.toJSON();
-	        var template = Handlebars.templates["resources"];
-	        this.$el.html(template({model: modelData, resources:collectionData}));
-	        return this;
-		},
+        render: function() {
+            var collectionData = this.collection.toJSON();
+            var modelData = this.model.toJSON();
+            var template = Handlebars.templates["resources"];
+            this.$el.html(template({model: modelData, resources:collectionData}));
+            return this;
+        },
 
-		postRender: function() {
-			this.listenTo(Adapt, 'drawer:triggerCustomView', this.remove);
-		},
+        postRender: function() {
+            this.listenTo(Adapt, 'drawer:triggerCustomView', this.remove);
+        },
 
-		onFilterClicked: function(event) {
-			event.preventDefault();
-			var $currentTarget = $(event.currentTarget);
-			if ($currentTarget.hasClass('selected')) {
-				return;
-			}
-			this.$('.resources-filter a').removeClass('selected');
-			var filter = $currentTarget.addClass('selected').attr('data-filter');
-			
-			if (filter === 'all') {
-				this.$('.resources-item').removeClass('display-none');
-				return;
-			}
-			this.$('.resources-item').removeClass('display-none').not("." + filter).addClass('display-none');
-		}
+        onFilterClicked: function(event) {
+            event.preventDefault();
+            var $currentTarget = $(event.currentTarget);
+            if ($currentTarget.hasClass('selected')) {
+                return;
+            }
+            this.$('.resources-filter a').removeClass('selected');
+            var filter = $currentTarget.addClass('selected').attr('data-filter');
+            
+            if (filter === 'all') {
+                var items = this.$('.resources-item').removeClass('display-none');
+                if (items.length === 0) return;
+                $(items[0]).a11y_focus();
+                return;
+            }
+            this.$('.resources-item').removeClass('display-none').not("." + filter).addClass('display-none');
+            var items = this.$('.resources-item.' + filter);
+            if (items.length === 0) return;
+            $(items[0]).a11y_focus();
+        }
 
-	});
+    });
 
-	return ResourcesView;
+    return ResourcesView;
 })
-	
+    
