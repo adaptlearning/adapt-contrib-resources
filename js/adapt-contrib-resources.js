d4397029a5891c7556b2fbad020b381ef9cc6f3d
Himanshu Rajotia <hrajotia@gmail.com>
2016-03-22 17:20:29 +0530

    minor code refactor

--- js/adapt-contrib-resources.js
+++ js/adapt-contrib-resources.js
@@ -1,34 +1,30 @@
-define(function(require) {
+define([
+    'backbone',
+    'coreJS/adapt',
+    './adapt-contrib-resourcesView',
+    './adapt-contrib-resourcesHelpers'
+], function(Backbone, Adapt, ResourcesView, ResourcesHelpers) {
 
-	var Adapt = require('coreJS/adapt');
-	var Backbone = require('backbone');
-	var ResourcesView = require('extensions/adapt-contrib-resources/js/adapt-contrib-resourcesView');
-	var ResourcesHelpers = require('extensions/adapt-contrib-resources/js/adapt-contrib-resourcesHelpers');
+    function setupResources(resourcesModel, resourcesItems) {
 
-	function setupResources(resourcesModel, resourcesItems) {
+        var resourcesCollection = new Backbone.Collection(resourcesItems);
+        var resourcesModel = new Backbone.Model(resourcesModel);
 
-		var resourcesCollection = new Backbone.Collection(resourcesItems);
-		var resourcesModel = new Backbone.Model(resourcesModel);
+        Adapt.on('resources:showResources', function() {
+            Adapt.drawer.triggerCustomView(new ResourcesView({
+                model: resourcesModel,
+                collection: resourcesCollection
+            }).$el);
+        });
 
-		Adapt.on('resources:showResources', function() {
-			Adapt.drawer.triggerCustomView(new ResourcesView({
-				model: resourcesModel, 
-				collection: resourcesCollection
-			}).$el);
-		});
-	
-	}
+    }
 
-	Adapt.once('app:dataReady', function() {
+    Adapt.once('app:dataReady', function() {
 
-		var courseResources = Adapt.course.get('_resources');
+        var courseResources = Adapt.course.get('_resources');
 
         // do not proceed until resource set on course.json
-        if (!courseResources) {
-            return console.log('Sorry, no resources object is set on the course.json file');
-        } else if (courseResources._isEnabled !== undefined && courseResources._isEnabled !== true) {
-            return console.log('Sorry, resources object is not enabled on the course.json file');
-        }
+        if (!courseResources || courseResources._isEnabled === false) return;
 
         var drawerObject = {
             title: courseResources.title,
@@ -39,8 +35,8 @@ define(function(require) {
         // Adapt.drawer.addItem([object], [callbackEvent]);
         Adapt.drawer.addItem(drawerObject, 'resources:showResources');
 
-		setupResources(courseResources, courseResources._resourcesItems);
+        setupResources(courseResources, courseResources._resourcesItems);
 
-	});
+    });
 
-})
+});
