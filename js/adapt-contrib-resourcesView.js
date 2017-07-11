215116a499d6a4f99ce62c5fc792d635b12c5eef
Ryan Lynch <ryanlynch@learningpool.com>
2017-07-11 17:51:28 +0100

    Issue-1643 - Added forceDownload property to example.json, added underscore as per configuration property convention, also using data-force-download as per data attributes convention. Updated readme.md

--- js/adapt-contrib-resourcesView.js
+++ js/adapt-contrib-resourcesView.js
@@ -50,7 +50,7 @@ define([
         onResourceClicked: function(event) {
             var data = $(event.currentTarget).data();
 
-            if (!data.forcedownload) {
+            if (!data.forceDownload) {
                 window.top.open(data.href);
                 return;
             }
