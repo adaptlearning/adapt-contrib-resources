e6c8ba84d36f73c5756b2247174bfb3edf3b5832
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2025-03-04 11:03:45 +0000

    review amends completed

--- migrations/v5.js
+++ migrations/v5.js
@@ -206,6 +206,67 @@ describe('Resources - v5.3.8 to v5.3.9', async () => {
   });
 });
 
+describe('Resources - v5.4.0 to v5.4.1', async () => {
+  let course;
+  whereFromPlugin('Resources - from v5.4.0', { name: 'adapt-contrib-resources', version: '<5.4.1' });
+
+  whereContent('Resources - where Resources', async (content) => {
+    course = getCourse();
+    return course?._resources;
+  });
+
+  mutateContent('Resources - add custom _filterButtons', async (content) => {
+    const customFilterButtons = { custom1: 'Custom category 1', custom2: 'Custom category 2', custom3: 'Custom category 3', custom4: 'Custom category 4', custom5: 'Custom category 5', custom6: 'Custom category 6', custom7: 'Custom category 7', custom8: 'Custom category 8', custom9: 'Custom category 9', custom10: 'Custom category 10' };
+    course._resources._filterButtons = { ...course._resources._filterButtons, ...customFilterButtons };
+    return true;
+  });
+
+  mutateContent('Resources - add custom _filterAria', async (content) => {
+    const customFilterArias = { custom1Aria: 'View custom category 1 links', custom2Aria: 'View custom category 2 links', custom3Aria: 'View custom category 3 links', custom4Aria: 'View custom category 4 links', custom5Aria: 'View custom category 5 links', custom6Aria: 'View custom category 6 links', custom7Aria: 'View custom category 7 links', custom8Aria: 'View custom category 8 links', custom9Aria: 'View custom category 9 links', custom10Aria: 'View custom category 10 links' };
+    course._resources._filterAria = { ...course._resources._filterAria, ...customFilterArias };
+    return true;
+  });
+
+  checkContent('Resources - check custom _filterButtons exist', async (content) => {
+    const isValid = _.has(course, '_resources._filterButtons.custom1');
+    if (!isValid) throw new Error('Resources - _resources._filterButtons custom buttons do not exist');
+    return true;
+  });
+
+  checkContent('Resources - check custom _filterAria exist', async (content) => {
+    const isValid = _.has(course, '_resources._filterAria.custom1Aria');
+    if (!isValid) throw new Error('Resources - _resources._filterAria custom buttons do not exist');
+    return true;
+  });
+
+  updatePlugin('Resources - update to v5.4.1', { name: 'adapt-contrib-resources', version: '5.3.9', framework: '>=5.19.1' });
+
+  testSuccessWhere('resources with course._resources._filterButtons/._filterAria', {
+    fromPlugins: [{ name: 'adapt-contrib-resources', version: '5.4.0' }],
+    content: [
+      { _type: 'course', _resources: { _filterButtons: { link: 'Link' }, _filterAria: { allAria: 'View all resources' } } }
+    ]
+  });
+
+  testSuccessWhere('resources with empty course._resources', {
+    fromPlugins: [{ name: 'adapt-contrib-resources', version: '5.4.0' }],
+    content: [
+      { _type: 'course', _resources: {} }
+    ]
+  });
+
+  testStopWhere('resources with empty course', {
+    fromPlugins: [{ name: 'adapt-contrib-resources', version: '5.4.0' }],
+    content: [
+      { _type: 'course' }
+    ]
+  });
+
+  testStopWhere('resources incorrect version', {
+    fromPlugins: [{ name: 'adapt-contrib-resources', version: '5.4.1' }]
+  });
+});
+
 describe('Resources - v5.4.3 to v5.5.0', async () => {
   let course;
   whereFromPlugin('Resources - from v5.4.3', { name: 'adapt-contrib-resources', version: '<5.5.0' });
